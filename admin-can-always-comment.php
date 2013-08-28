0ad690da4d4d6eeded38842ed57f565e5a311678
Mikko Virenius <info@arkimedia.fi>
2013-08-28 22:41:11 +0300

    Add filter description

--- admin-can-always-comment.php
+++ admin-can-always-comment.php
@@ -11,6 +11,12 @@ License: GPL2
 
 add_filter( 'comments_open', 'admin_can_always_comment', 10, 2 );
 
+/**
+ * If current user is an admistrator, set commenting status to open
+ * @param  boolean $open Default status
+ * @param  int $post_id Post ID
+ * @return boolean $open Filtered status
+ */
 function admin_can_always_comment( $open, $post_id ) {
 
 	if ( current_user_can('manage_options') )
